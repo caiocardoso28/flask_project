f672a6149ea0ff0141b0d71f0f07d3f1484278e3
caiocardoso28 <caioleitecardoso@gmail.com>
2023-01-11 20:50:29 -0500

    Added a test.js file to start animating 3d

--- application/static/test.js
+++ application/static/test.js
@@ -1,5 +1,6 @@
 // Set up the scene, camera, and renderer
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -60,3 +61,4 @@ function animate() {
 }
 
 animate();
+
